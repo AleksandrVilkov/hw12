724c2c3294cb50da60d27953af2c0251eed2f072
Вилков Александр Сергеевич <ASVilkov@sberbank.ru>
2024-12-01 13:43:41 +0300

    hw12

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,10 +3,14 @@ package org.example;
 
 import lombok.SneakyThrows;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class HomeWork {
+
+    private static final char LEFT = 'L';
 
     /**
      * <h1>Задание 1.</h1>
@@ -14,8 +18,30 @@ public class HomeWork {
      */
     @SneakyThrows
     public void frogSteps(InputStream in, OutputStream out) {
+        try (var reader = new BufferedReader(new InputStreamReader(in));
+             var writer = new BufferedWriter(new OutputStreamWriter(out))) {
+            var queue = reader.lines().collect(Collectors.toCollection(LinkedList::new));
+            var countSteps = Integer.parseInt(Objects.requireNonNull(queue.poll()));//Извлекаем что бы не мешал
+            while (!queue.isEmpty()) writer.write(minimumPossibleValue(queue.poll()) + "\n");
+            writer.flush();
+        }
     }
 
-
-
+    private int minimumPossibleValue(String line) {
+        int max = 0;
+        boolean hasLeft = line.contains(String.valueOf(LEFT));
+        int count = hasLeft ? 1 : 0;
+        var directions = line.toCharArray();
+        for (int i = 1; i < directions.length; i++) {
+            if (directions[i] == LEFT) {
+                if (directions[i - 1] == directions[i]) {
+                    count++;
+                    continue;
+                }
+                max = Math.max(max, count);
+                count = hasLeft ? 1 : 0;
+            }
+        }
+        return Math.max(max, count) + 1;
+    }
 }
